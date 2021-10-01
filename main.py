ea2ff55bb035e9b28aaa593a859e0596beea7348
saditya7 <64976785+saditya7@users.noreply.github.com>
2021-10-02 01:08:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,3 +81,78 @@ def o365enum_autodiscover(usernames):
                 state = 1
                 break
         print("{},{}".format(username, state))
+
+def o365enum_office(usernames):
+    '''
+    Checks if `usernames` exists using office.com method.
+
+    Args:
+        usernames(list): list of usernames to enumerate
+    '''
+    headers = {
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"\
+            " (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+    }
+    # first we open office.com main page
+    session = requests.session()
+    response = session.get(
+        "https://www.office.com",
+        headers=headers
+    )
+    # we get the application identifier and session identifier
+    client_id = re.findall(b'"appId":"([^"]*)"', response.content)
+    # then we request the /login page which will redirect us to the authorize
+    # flow
+    response = session.get(
+        "https://www.office.com/login?es=Click&ru=/&msafed=0",
+        headers=headers,
+        allow_redirects=True
+    )
+    hpgid = re.findall(b'hpgid":([0-9]+),', response.content)
+    hpgact = re.findall(b'hpgact":([0-9]+),', response.content)
+
+    if not client_id or not hpgid or not hpgact:
+        raise Exception("An error occured when generating headers.")
+
+    # we setup the right headers to blend in
+    headers['client-request-id'] = client_id[0]
+    headers['Referer'] = response.url
+    headers['hpgrequestid'] = response.headers['x-ms-request-id']
+    headers['canary'] = ''.join(
+        random.choice(
+            string.ascii_uppercase + string.ascii_lowercase + string.digits + "-_"
+        ) for i in range(248)
+    )
+    headers['hpgid'] = hpgid[0]
+    headers['Accept'] = "application/json"
+    headers['hpgact'] = hpgact[0]
+    headers['Origin'] = "https://login.microsoftonline.com"
+
+    # we setup the base JSON object to submit
+    payload = {
+        "isOtherIdpSupported":True,
+        "checkPhones":False,
+        "isRemoteNGCSupported":True,
+        "isCookieBannerShown":False,
+        "isFidoSupported":False,
+        "originalRequest": re.findall(b'"sCtx":"([^"]*)"', response.content)[0].decode('utf-8'),
+        "forceotclogin":False,
+        "isExternalFederationDisallowed":False,
+        "isRemoteConnectSupported":False,
+        "federationFlags":0,
+        "isSignup":False,
+        "isAccessPassSupported":True
+    }
+
+    for username in usernames:
+        payload["username"] = username
+        response = session.post(
+            "https://login.microsoftonline.com/common/GetCredentialType?mkt=en-US",
+            headers=headers,
+            json=payload
+        )
+        if response.status_code == 200:
+            exists = not int(response.json()['IfExistsResult'])
+        else:
+            exists = -1
+        print("{},{}".format(username, int(exists)))
